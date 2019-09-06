0c874510bfcb15fa09c04a168244090b6caa4cd0
Chris Commisso <cfc5762@rit.edu>
2019-09-05 21:08:02 -0400

    everything

---
+++ Veracity/Launcher/Assets/Button.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public int buttonnum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            switch (buttonnum)
+            {
+                case 0:
+                    OpenApps.Audio = true;
+                    break;
+                case 1:
+                    OpenApps.Youtube = true;
+                    break;
+                case 2:
+                    OpenApps.Job = true;
+                    break;
+                case 3:
+                    OpenApps.Fun = true;
+                    break;
+                case 4:
+                    OpenApps.Nature = true;
+                    break;
+                case 5:
+                    OpenApps.ABC = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- Veracity/Launcher/Assets/OpenApps.cs
+++ Veracity/Launcher/Assets/OpenApps.cs
@@ -5,7 +5,13 @@ using System.Diagnostics;
 using UnityEngine;
 
 public class OpenApps : MonoBehaviour {
-    Process current;
+    public static bool RecRoom;
+    public static bool Youtube;
+    public static bool Job;
+    public static bool Fun;
+    public static bool Audio;
+    public static bool Nature;
+    public static bool ABC;
     public void Open(string name)
     {
         if (name == "RecRoom")
@@ -15,6 +21,26 @@ public class OpenApps : MonoBehaviour {
         else if (name == "Youtube")
         {
             Process.Start("C:\\Program Files (x86)\\Steam\\steamapps\\common\\YouTubeVR\\YouTubeVR.exe");
+        }
+        else if (name == "Audio")
+        {
+            Process.Start("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Audioshield\\Audioshield.exe");
+        }
+        else if (name == "Job")
+        {
+            Process.Start("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Job Simulator\\JobSimulator.exe");
+        }
+        else if (name == "Fun")
+        {
+            Process.Start("C:\\Program Files (x86)\\Steam\\steamapps\\common\\NVIDIA VR Funhouse\\VRFunhouse.exe");
+        }
+        else if (name == "Nature")
+        {
+            Process.Start("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Nature Treks VR\\Nature Treks VR.exe");
+        }
+        else if (name == "ABCPaint")
+        {
+            Process.Start("C:\\Program Files (x86)\\Steam\\steamapps\\common\\ABC Paint\\ABCPaint.exe");
         }
     }
     public void Close(string name)
@@ -47,6 +73,76 @@ public class OpenApps : MonoBehaviour {
                 //dont do that
             }
         }
+        else if (name == "Job")
+        {
+            try
+            {
+                foreach (Process proc in Process.GetProcessesByName("JobSimulator"))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                //dont do that
+            }
+        }
+        else if (name == "Nature")
+        {
+            try
+            {
+                foreach (Process proc in Process.GetProcessesByName("Nature Treks VR"))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                //dont do that
+            }
+        }
+        else if (name == "ABCPaint")
+        {
+            try
+            {
+                foreach (Process proc in Process.GetProcessesByName("ABCPaint"))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                //dont do that
+            }
+        }
+        else if (name == "Audio")
+        {
+            try
+            {
+                foreach (Process proc in Process.GetProcessesByName("Audiosheild"))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                //dont do that
+            }
+        }
+        else if (name == "Fun")
+        {
+            try
+            {
+                foreach (Process proc in Process.GetProcessesByName("VRFunhouse"))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                //dont do that
+            }
+        }
     }
 	// Use this for initialization
 	void Start () {
@@ -56,23 +152,96 @@ public class OpenApps : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown("r"))
+        if (Input.GetKeyDown("r")||RecRoom)
         {
             if (Process.GetProcessesByName("Recroom_Release").Length > 0)
+            {
+                RecRoom = false;
                 Close("RecRoom");
+            }
             else
             {
+                RecRoom = false;
                 Open("RecRoom");
             }
         }
-        if (Input.GetKeyDown("y"))
+        if (Input.GetKeyDown("y")||Youtube)
         {
             if (Process.GetProcessesByName("YouTubeVR").Length > 0)
+            {
+                Youtube = false;
                 Close("Youtube");
+            }
             else
             {
+                Youtube = false;
                 Open("Youtube");
             }
         }
-	}
+        if (Input.GetKeyDown("n")||Nature)
+        {
+            if (Process.GetProcessesByName("Nature Treks VR").Length > 0)
+            {
+                Nature = false;
+                Close("Nature");
+            }
+            else
+            {
+                Nature = false;
+                Open("Nature");
+            }
+        }
+        if (Input.GetKeyDown("f")||Fun)
+        {
+            if (Process.GetProcessesByName("VRFunhouse").Length > 0)
+            {
+                Fun = false;
+                Close("Fun");
+            }
+            else
+            {
+                Fun = false;
+                Open("Fun");
+            }
+        }
+        if (Input.GetKeyDown("p")||ABC)
+        {
+            if (Process.GetProcessesByName("ABCPaint").Length > 0)
+            {
+                ABC = false;
+                Close("ABCPaint");
+            }
+            else
+            {
+                ABC = false;
+                Open("ABCPaint");
+            }
+        }
+        if (Input.GetKeyDown("a")||Audio)
+        {
+            if (Process.GetProcessesByName("Audiosheild").Length > 0)
+            {
+                Audio = false;
+                Close("Audio");
+            }
+            else
+            {
+                Audio = false;
+                Open("Audio");
+            }
+        }
+        if (Input.GetKeyDown("j")||Job)
+        {
+            if (Process.GetProcessesByName("JobSimulator").Length > 0)
+            {
+                Job = false;
+                Close("Job");
+            }
+            else
+            {
+                Job = false;
+                Open("Job");
+            }
+        }
+    }
 }
